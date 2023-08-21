8d24bca4289590366e8b3154f5b4aa014fa7ee31
Haider Rizvi <>
2023-08-22 03:16:42 +0500

    improve readability with proper method

--- SwiftEssentialDeveloperTests/RemoteFeedLoaderTests.swift
+++ SwiftEssentialDeveloperTests/RemoteFeedLoaderTests.swift
@@ -40,7 +40,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         sut.load {captureError.append($0)  }
         
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(captureError, [.connectivity])
     }
@@ -62,5 +62,10 @@ class RemoteFeedLoaderTests: XCTestCase {
             completions.append(completion)
             requestedURLs.append(url)
         }
+        
+        func complete(with error: Error, at index: Int = 0){
+            completions[index](error)
+        }
     }
+  
 }
